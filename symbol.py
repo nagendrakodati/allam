39b5078466c7c1b627aacbef24f6bccd8497ec33
nagendrakodati <115443584+nagendrakodati@users.noreply.github.com>
2023-03-01 17:58:09 +0530

    init

---
+++ symbol.py
@@ -0,0 +1,32 @@
+from turtle import*
+color('black','blue')
+begin_fill()
+
+pensize(10)
+circle(65)
+circle(65 ,steps=3)
+circle(65 ,steps=10)
+circle(65 ,steps=9)
+end_fill()
+bgcolor('red')
+setposition(200,55)
+left(75)#1
+forward(150)
+left(45)#2
+forward(120)
+left(60)#3
+forward(95)
+left(50)#4
+forward(95)
+right(48)#5
+forward(75)
+right(50)#6
+forward(95)
+left(48)#7
+forward(90)
+left(60)#8
+forward(120)
+left(45)#9
+forward(130)
+left(58)#10
+forward(200)
